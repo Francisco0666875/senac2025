7624b5f9309ff5881d6f49892a7b7b28c4f40345
Francisco0666875 <02551522056@senacrs.edu.br>
2025-05-22 14:59:34 -0300

    Add files via upload

---
+++ dobro.java
@@ -0,0 +1,8 @@
+public class dobro {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            int dobro = i * 2;
+            System.out.println("O dobro de " + i + " é " + dobro);
+        }
+    }
+}
---
+++ squad.java
@@ -0,0 +1,8 @@
+public class squad {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            int quadrado = i * i;
+            System.out.println("O quadrado de " + i + " é " + quadrado);
+        }
+    }
+}
